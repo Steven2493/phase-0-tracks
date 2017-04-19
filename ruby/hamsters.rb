92e27a6f7f5dba610caeed1bb3e3d71131d077b3
Steven Aguilar <Stevenaguilar24@gmail.com>
2017-04-18 22:14:43 -0400

    Added Hamster question to hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,25 @@
+print "Which hamster would you like?"
+ham_name = gets.chomp
+puts "How loud would like it to be?"
+vol_level = gets.chomp.to_i
+
+puts "What fur color would you like?"
+fur_color = gets.chomp
+
+puts "is this hamster a good choice(yes/no)?"
+adoption = gets.chomp
+
+puts "Can you guess the age of this hamster?"
+est_age = gets.chomp.to_i
+
+
+puts"Summary:"
+puts"Hamster Name: #{ham_name}"
+puts"Volume Level: #{vol_level}"
+puts"Fur color: #{fur_color}"
+puts"Good candidate: #{adoption}"
+if est_age > 0
+	puts "This hamster is #{est_age} years old"
+else
+	puts "Customer Didn't guess question "
+end
