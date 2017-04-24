92d581f718b5513e46315b5365400052ce6a8ac2
Steven Aguilar <Stevenaguilar24@gmail.com>
2017-04-24 00:48:22 -0400

    Added allergy loop and put statement once loop ends

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,11 +4,14 @@ puts "How many employees will be processed?"
 process = gets.chomp.to_i
 count = 0
 
+#Loops until it reaches inputed applicants number
 while count < process
 puts "Hello whats is your name?"
 name = gets.chomp
 if name == "Drake Cula" || name == "Tu Fang"
-	name = false
+	nameCheck = false
+else
+	nameCheck = true
 end
 
 puts "How old are you?"
@@ -17,9 +20,9 @@ age = gets.chomp.to_i
 puts "What year were you born?"
 yearOfBirth = gets.chomp.to_i
 if DateTime.now.year - yearOfBirth == age
-	 age = true
+	 ageCheck = true
 else
-	 age = false
+	 ageCheck = false
 end
 puts "Should we order you some garlic bread?(Y/N)"
 
@@ -29,14 +32,24 @@ puts "Would you like to enroll in the company's health insurance?(Y/N)"
 
 healthAnswer = gets.chomp.downcase
 
+#Loops to find sunshine string
+allergy = nil
+until allergy == "sunshine" || allergy == "done"
+	puts "Do you have any allergys?"
+	allergy = gets.chomp.downcase
+	if allergy == "sunshine"
+		puts "probably a vampire"
+	end
+end
 
-if age == true && (breadAnswer == "y" || healthAnswer == "y") && name
+#Checks Answer to see if they are a vampire
+if ageCheck == true && (breadAnswer == "y" || healthAnswer == "y") && nameCheck
 	puts "Probably not a vampire"
-elsif age == false && (breadAnswer == "n" || healthAnswer == "n")
+elsif ageCheck == false && (breadAnswer == "n" || healthAnswer == "n")
 	puts "Probably a vampire"
-elsif age == false && breadAnswer == "n" && healthAnswer == "n"
+elsif ageCheck == false && breadAnswer == "n" && healthAnswer == "n"
 	puts "Almost certainly a vampire"
-elsif age == true && (breadAnswer == "y" || healthAnswer == "y") && !name
+elsif ageCheck == true && (breadAnswer == "y" || healthAnswer == "y") && !nameCheck
 	puts "Definitely a vampire"
 else
 	puts "Results inconclusive" 
@@ -44,3 +57,6 @@ end
 
 count += 1
 end
+
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
