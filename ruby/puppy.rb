7ac74209f806cf1e0765355db01dd70de3c688fa
Steven Aguilar <Stevenaguilar24@gmail.com>
2017-05-03 18:04:16 -0400

    Added Puppy characteristics and Behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#Characteristics---------------- 
+
+#Friendly: yes
+#Name: Varies
+#color: Varies
+#Size: Varies
+#Coat Texture: Varies
+
+#Behavior------------------
+
+#Energetic 
+#Licks
+#Loves Attention
+#Outgoing
