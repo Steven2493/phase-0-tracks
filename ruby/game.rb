0b9861f07666711e8b53c1248c4bb944223d1187
Steven Aguilar <Stevenaguilar24@gmail.com>
2017-05-07 18:49:41 -0400

    Added Driver code and updated user check method

--- ruby/game.rb
+++ ruby/game.rb
@@ -12,18 +12,61 @@
 
 
 class Word_game
-	attr_reader :guess_count, :word_tobe_guess, :user_guess, :game_over
-
-	def initialize(word)
-		@word_tobe_guess = word
-		@guess_count = 0
-		@game_over = false
-	end
-
-	
+attr_accessor :guess_count, :word_tobe_guess, :game_over, :used_letter, :guess_checker, :dashes, :user_guess
 
 
-
-
+def initialize(word)
+@word_tobe_guess = word
+@user_guess = player2_guess
+@guess_count = 0
+@game_over = false
+@guess_checker = false
+@dashes = []
+@used_letter = []
 end
 
+  def checks_input
+
+    word_index = dashes
+    word_array = @word_tobe_guess.split('')
+    user_array = @user_guess.split('')
+
+    user_array.each do |letter| # Compares all letters
+     if word_array.include?(letter)
+      match_index = word_array.index(letter)
+      word_index[match_index] = letter
+     end
+    end 
+    p dashes = word_index.join('')
+  end
+  
+  def history_checker(guess)
+    if @used_letter.include?(guess)
+      puts "You already used that letter"
+    else
+      @guess_count += 1 
+      @used_letter << guess
+    end
+    
+  end
+  
+end
+
+
+#Driver Code
+new_game = Word_game.new("hello") #Word to be guess
+new_game.dashes = ("-" * new_game.word_tobe_guess.length).split('') #Gets length of word and makes it to (-)
+
+
+while new_game.game_over != true
+  num_guess = nil
+  
+  if new_game.game_over != true && num_guess != 0 
+    num_guess = new_game.word_tobe_guess.length- new_game.guess_count
+    puts "You have #{num_guess} to guess the correct word:"
+    new_game.user_guess = gets.chomp.downcase
+    new_game.checks_input
+    new_game.history_checker(new_game.user_guess)
+   end
+  @game_over
+end
