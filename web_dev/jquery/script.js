8d5cf4b0c1a9c31111042c3ce882832fd18ec42c
Steven Aguilar <Stevenaguilar24@gmail.com>
2017-05-30 00:27:37 -0400

    Added Jquery buttons

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-	$("#change_background").on("click", function() {
-		$("body").css("background-color","red");
+	$("#red_background").on("click", function() {
+	$("body").css("background-color","red");
+	});
+
+	$("#blue_background").on("click", function() {
+	$("body").css("background-color","blue");
+	});
+
+	$("#green_background").on("click", function() {
+	$("body").css("background-color","green");
+	});
+
+	$("#black_background").on("click", function() {
+	$("body").css("background-color","black");
 	});
 });
